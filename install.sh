1c2ca82a728f7f06eff3a5cb9a3fa9848a1740c4
ktw361 <zhifanzhu@yeah.net>
2021-09-25 20:08:01 +0800

    update instal.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,17 @@
+cur_dir=$(cd $(dirname "$0") && pwd)
+
 for fullpath in $PWD/*
 do
 	f="$(basename -- $fullpath)"
 	if ! [ "$f" = "install.sh" ]; then
         target=~/.$f
-		echo "Installing to $target"
         if [[ -f "$target" ]]; then
             backup=~/.$f.backup
             echo "Backup old -> $backup"
             cp ~/.$f ~/.$f.backup
         fi
-        ln -sf ~/.dotfiles/$f ~/.$f
+        echo "ln -sf ${cur_dir}/$f ~/.$f"
+        ln -sf ${cur_dir}/$f ~/.$f
         echo ""
 	fi
 done
