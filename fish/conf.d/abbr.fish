8a4545ed8a7c32d004e62a6822965d5272cb2fa6
Zhifan Zhu <ktw361@gmail.com>
2023-02-05 21:45:19 +0000

    add abbrs

---
+++ fish/conf.d/abbr.fish
@@ -0,0 +1,6 @@
+abbr -a gs "git status"
+abbr -a ga "git add"
+abbr -a gc "git commit"
+abbr -a gd "git diff"
+abbr -a gl "git log"
+abbr -a glols "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --stat"
