e9bd4185d582de731d09ced989517dc62f3fad90
Zhifan Zhu <zhifan.zhu@bristol.ac.uk>
2023-03-06 00:07:09 +0000

    update

--- fish/functions/dependent_run.fish
+++ fish/functions/dependent_run.fish
@@ -3,7 +3,7 @@ function dependent_run
     set cmd $argv[2]
 	while kill -0 $pid 2> /dev/null;
 		set time (date)
-		echo "At ${time}, process is still running, sleep..."
+		echo "At $time, process is still running, sleep..."
 		sleep 3600
     end
 	echo "Now run command:"
