e3dd5b289f8000470bdc4ff8ce26ab52f88d9d23
Zhifan Zhu <zhifan.zhu@bristol.ac.uk>
2023-03-06 00:04:48 +0000

    fish: add dependent_run

---
+++ fish/functions/dependent_run.fish
@@ -0,0 +1,13 @@
+function dependent_run
+	set pid $argv[1]
+    set cmd $argv[2]
+	while kill -0 $pid 2> /dev/null;
+		set time (date)
+		echo "At ${time}, process is still running, sleep..."
+		sleep 3600
+    end
+	echo "Now run command:"
+	echo $cmd
+	echo ""
+	eval $cmd
+end
