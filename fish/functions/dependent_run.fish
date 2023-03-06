3af855e990c2c49f15c1608441f7a89b8cafa4d7
Zhifan Zhu <zhifan.zhu@bristol.ac.uk>
2023-03-06 00:12:41 +0000

    fish:dependent_fun add custom wait time

--- fish/functions/dependent_run.fish
+++ fish/functions/dependent_run.fish
@@ -1,10 +1,11 @@
 function dependent_run
 	set pid $argv[1]
     set cmd $argv[2]
+    set wait $argv[3]
 	while kill -0 $pid 2> /dev/null;
 		set time (date)
 		echo "At $time, process is still running, sleep..."
-		sleep 3600
+		sleep $wait
     end
 	echo "Now run command:"
 	echo $cmd
