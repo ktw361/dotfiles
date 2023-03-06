6b11f6ff194caa1e3ee8a83c20713320ea68a90c
Zhifan Zhu <zhifan.zhu@bristol.ac.uk>
2023-03-06 11:34:22 +0000

    fish: add abbr

--- configure-fish.sh
+++ configure-fish.sh
@@ -3,4 +3,5 @@ mkdir -p $HOME/.config/fish/functions
 mkdir -p $HOME/.config/fish/conf.d
 
 ln fish/functions/fish_prompt.fish $HOME/.config/fish/functions/fish_prompt.fish
+ln fish/functions/dependent_run.fish $HOME/.config/fish/functions/dependent_run.fish
 ln fish/conf.d/abbr.fish $HOME/.config/fish/conf.d/abbr.fish
