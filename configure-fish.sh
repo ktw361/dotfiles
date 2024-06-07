690de8b2d5c70d9eb69124c9aa2593fceb6e4a54
Zhifan Zhu <zhifan.zhu@bristol.ac.uk>
2024-06-07 12:34:26 +0100

    update

--- configure-fish.sh
+++ configure-fish.sh
@@ -2,6 +2,6 @@
 mkdir -p $HOME/.config/fish/functions
 mkdir -p $HOME/.config/fish/conf.d
 
-ln fish/functions/fish_prompt.fish $HOME/.config/fish/functions/fish_prompt.fish
-ln fish/functions/dependent_run.fish $HOME/.config/fish/functions/dependent_run.fish
-ln fish/conf.d/abbr.fish $HOME/.config/fish/conf.d/abbr.fish
+ln -sfn `realpath fish/functions/fish_prompt.fish` $HOME/.config/fish/functions/fish_prompt.fish
+ln -sfn `realpath fish/functions/dependent_run.fish` $HOME/.config/fish/functions/dependent_run.fish
+ln -sfn `realpath fish/conf.d/abbr.fish` $HOME/.config/fish/conf.d/abbr.fish
